f03858d1864726f3df00685e4741ace4686cd7d8
jitendra <jeetvaishnavnadol@gmail.com>
2021-09-07 09:53:22 +0530

    login page

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,39 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted ">
+    <!-- Section: Social media -->
+   
+    <!-- Section: Social media -->
+  
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <p>panel content here</p>
+          </div>
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+        
+          <!-- Grid column -->
+  
+          <!-- Grid column -->
+        
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+  
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2021 Copyright:
+      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
